b7b227bb70ad937859d5b27539ddbebe69d755b7
Adithya S Narasinghe <adithyasnarasinghe@gmail.com>
2018-10-05 01:56:14 +0530

    Animations & News Detail on Safari

--- FB Workshop/NewsViewController.swift
+++ FB Workshop/NewsViewController.swift
@@ -28,13 +28,44 @@ class NewsViewController: UIViewController {
                 self.news = news!
                 if self.tblNews != nil {
                     self.tblNews.reloadData()
+                    self.animateTable()
                 }
             }
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        animateTable()
+    }
+    
     func setup(interest: String) {
         self.titleStr = interest
+    }
+    
+    func animateTable() {
+        tblNews.reloadData()
+        let cells = tblNews.visibleCells
+        
+        let tableViewHeight = tblNews.bounds.size.height
+        
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableViewHeight)
+        }
+        
+        var delayCounter = 0
+        for cell in cells {
+            UIView.animate(withDuration: 1.75, delay: Double(delayCounter) * 0.05, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                cell.transform = CGAffineTransform.identity
+            }, completion: nil)
+            delayCounter += 1
+        }
+    }
+    
+    @objc func showDetails(sender: UIView) {
+        if let link = URL(string: self.news[sender.tag].url) {
+            UIApplication.shared.open(link)
+        }
     }
 
 }
@@ -48,7 +79,15 @@ extension NewsViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NewsItem",
                                                       for: indexPath) as! NewsTableViewCell
         cell.render(news: self.news[indexPath.row])
+        cell.view.tag = indexPath.row
+        cell.view.addTarget(self, action: #selector(showDetails), for: .touchUpInside)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let link = URL(string: self.news[indexPath.row].url) {
+            UIApplication.shared.open(link)
+        }
     }
     
     
